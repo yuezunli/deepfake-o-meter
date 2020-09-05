4fccc1869688376b45ff52922990307c4d139837
Yuezun Li <liyuezun@gmail.com>
2020-09-05 09:30:27 -0400

    update

--- server_demo.py
+++ server_demo.py
@@ -2,7 +2,7 @@ import requests
 import cv2
 
 
-im = cv2.imread('test_img.png')
+im = cv2.imread('Selection_001.png')
 url = 'http://169.226.117.97:2500/deepforensics'
 resp = requests.post(url, json={'feature': im.tolist()}).json()
 print(resp)
